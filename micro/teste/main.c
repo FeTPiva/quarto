b10780a1e8dc62fd3883d073bbb3c03ea0c289e7
Aluno <Aluno>
2018-03-07 16:48:02 -0300

    akljdfkld

---
+++ micro/teste/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+//relembrando os treco
+
+int funcao(int a){
+printf("digite: \n");
+scanf("%d", &a);
+printf("%d", a*10 );
+}
+
+
+int main()
+{
+    printf("Hello world!\n");
+    int a;
+    printf("Color %s,Number %d , Float %4.2f \n", "blue", 1, 3.22  );
+
+    funcao(a);
+    if(a=1){
+
+    }
+
+
+
+
+    return 0;
+
+}
